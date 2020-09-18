732478318a4505f375228681b57925dbcc63f267
Jeremiah Flaga <flaga.jeremiah@gmail.com>
2020-09-18 12:44:26 +0800

    code for "Experimenting with ASP.NET Core Authentication Schemes" article by Scott Allen (supposed to be committed last Sept 11-14)

---
+++ 2020-09-11-aspnet-authentication-schemes/README.md
@@ -0,0 +1,22 @@
+﻿# Code for ["Experimenting with ASP.NET Core Authentication Schemes"](https://odetocode.com/blogs/scott/archive/2019/01/02/experimenting-with-asp-net-core-authentication-schemes.aspx) by Scott Allen
+
+Was trying to have an answer to this question: https://github.com/dotnet-architecture/eShopOnContainers/issues/1423
+
+<blockquote markdown="1">
+I can see this in the `AccountController` of the Web MVC project
+
+``` csharp
+[Authorize(AuthenticationSchemes = "OpenIdConnect")]
+public class AccountController : Controller
+{
+    ...
+```
+
+I tried to search for an explanation for that and found this: https://docs.microsoft.com/en-us/aspnet/core/security/authorization/limitingidentitybyscheme?view=aspnetcore-3.1
+
+But the example there uses constant strings, like `JwtBearerDefaults.AuthenticationScheme`, instead of a literal string, like `"OpenIdConnect"`.
+
+May I ask why that is the case? Is there no string constant for `"OpenIdConnect"`? How would one know which strings to put in there?
+
+Thanks.
+</blockquote>
---
+++ 2020-09-11-aspnet-authentication-schemes/SampleSite/Startup.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace SampleSite
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "cookie1";
+            })
+            .AddCookie("cookie1", "cookie1", options =>
+            {
+                options.Cookie.Name = "cookie1";
+                options.LoginPath = "/loginc1";
+            })
+            .AddCookie("cookie2", "cookie2", options =>
+            {
+                options.Cookie.Name = "cookie2";
+                options.LoginPath = "/loginc2";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.Use(next =>
+            {
+                return async ctx =>
+                {
+                    switch (ctx.Request.Path)
+                    {
+                        case "/loginc1":
+                            var identity1 = new ClaimsIdentity("cookie1");
+                            identity1.AddClaim(new Claim("name", "Alice-c1"));
+                            await ctx.SignInAsync("cookie1", new ClaimsPrincipal(identity1));
+                            break;
+                        case "/loginc2":
+                            var identity2 = new ClaimsIdentity("cookie2");
+                            identity2.AddClaim(new Claim("name", "Alice-c2"));
+                            await ctx.SignInAsync("cookie2", new ClaimsPrincipal(identity2));
+                            break;
+                        case "/logoutc1":
+                            await ctx.SignOutAsync("cookie1");
+                            break;
+                        case "/logoutc2":
+                            await ctx.SignOutAsync("cookie2");
+                            break;
+                        default:
+                            await next(ctx);
+                            break;
+                    }
+                };
+            });
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
